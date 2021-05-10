e1aaedb64caac9308775c175ef91931ed44ce22e
Aleia DeVore <2496@holbertonschool.com>
2021-05-10 19:34:25 +0000

    CORS added

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,10 +4,12 @@
 from api.v1.views import app_views
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS
 from os import getenv
 from models import storage
 import os
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 HBNB_API_HOST = getenv('HBNB_API_HOST')
 if HBNB_API_HOST is None:

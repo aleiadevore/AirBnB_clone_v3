06d1e5df605fbb95aa74b6c33c1c904d899b367f
Aleia DeVore <2496@holbertonschool.com>
2021-05-10 04:06:08 +0000

    Untested

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Default restful api actions for state"""
+
+from api.v1.views import app_views
+from flask import abort, Flask, jsonify, request
+from models import storage
+from models.state import State
+
+
+@app_views.route('/amenities', strict_slashes=False, methods=['GET'])
+@app_views.route('/amenities/<amenity_id>',
+                 strict_slashes=False, methods=['GET'])
+def get_amenity(amenity_id=None):
+    """Method to retrieve list of all amenity objects or state object by id"""
+    if amenity_id is None:
+        amenity_list = []
+        for obj in storage.all(Amenity).values():
+            state_list.append(obj.to_dict())
+        return jsonify(amenity_list)
+    for obj in storage.all(Amenity).values():
+        if obj.id == state_id:
+            state = obj.to_dict()
+            return state
+    abort(404)
+
+
+@app_views.route('/amenities/<amenity_id>',
+                 strict_slashes=False, methods=['DELETE'])
+def del_amenities(amenity_id):
+    """Deletes amenity object by id"""
+    for obj in storage.all(Amenity).values():
+        if obj.id == amenity_id:
+            obj.delete()
+            storage.save()
+            return ({}, 200)
+    abort(404)
+
+
+@app_views.route('/amenities', strict_slashes=False, methods=['POST'])
+def post_state():
+    """Method that transforms HTTP body request to a new amenity"""
+    jreq = request.get_json(silent=True)
+
+    if jreq is None:
+        abort(400, 'Not a JSON')
+    if 'name' not in jreq:
+        abort(400, 'Missing name')
+    new_amenity = Amenity(**jreq)
+    storage.save()
+    return jsonify(new_amenity.to_dict()), 201
+
+
+@app_views.route('/amenities/<amenity_id>',
+                 strict_slashes=False, methods=['PUT'])
+def put_state(amenity_id):
+    """Update state object"""
+    for obj in storage.all(Amenity).values():
+        if obj.id == amenity_id:
+            amenity = obj.to_dict()
+            jreq = request.get_json(silent=True)
+            if jreq is None:
+                abort(400, 'Not a JSON')
+            for key, value in jreq.items():
+                if key == 'id' or key == "created_at" or key == "updated_at":
+                    continue
+                setattr(obj, key, value)
+            obj.save()
+            return jsonify(obj.to_dict()), 200
+    abort(404)
